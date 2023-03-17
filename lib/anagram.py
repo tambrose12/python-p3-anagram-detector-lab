102d0fc02ad1c3ca468c3ca1acb4bd9810a73f9a
Taylor Ambrose <taykesby@gmail.com>
2023-03-17 11:00:09 -0400

    lab complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.matching_words = []
+
+    def match(self, input_list):
+        for each_word in input_list:
+            if sorted(list(each_word)) == sorted(list(self.word)):
+                self.matching_words.append(each_word)
+
+        return self.matching_words
+
+        # for each_word in input_list:
+        #     if set(each_word) in input_list == set(self.word):
+        #         print(self.matching_words.append(each_word))
